f4ea3f5ff5a65122314a5bfefe70c11736bbf259
Dario Di Gulio <ddigulio@technisys.com>
2020-08-05 19:13:01 -0300

    return array to json

--- app.py
+++ app.py
@@ -25,21 +25,14 @@ def getPortafolioArg(pais='argentina'):
         abort(400, description="Params not found")
 
 
-@app.route('/titulos/<titulo>')
-def getTitulos(titulo=0):
-    if int(titulo) < 14:
-        titulos = Titulos().getTitulos(int(titulo))
-    else: 
-        abort(400, description="Params not found")
-    return titulos
+@app.route('/titulos')
+def getTitulos():
+    return Titulos().getTitulos()
 
 
-@app.route('/cotizaciones/<pais>/<titulo>')
-def getCotizaciones(pais='argentina', titulo=0):
-    if pais in COUNTRIES and int(titulo) < 5:
-        return Titulos().getCotizaciones(str(pais),int(titulo))
-    else:
-        abort(400, description="Params not found")
+@app.route('/cotizaciones/<pais>')
+def getCotizaciones(pais='argentina'):
+    return Titulos().getCotizaciones(str(pais))
 
 
 if __name__ == '__main__':
--- controllers/Titulos.py
+++ controllers/Titulos.py
@@ -2,6 +2,8 @@ import requests
 from controllers.Token import *
 from util.constants import URL_API
 import json
+from json import dumps
+from flask import Flask, make_response
 
 
 class Titulos(object):
@@ -10,11 +12,11 @@ class Titulos(object):
         self.headers = {'Authorization': f'Bearer {Token.getToken()}'}
 
 
-    def getTitulos(self, titulo):
+    def getTitulos(self):
         response = requests.get(f'{URL_API}/Titulos/FCI', headers=self.headers)
-        return response.json()[titulo]
+        return make_response(dumps(response.json()))
 
 
-    def getCotizaciones(self, pais, titulo):
+    def getCotizaciones(self, pais):
         response = requests.get(f'{URL_API}/{pais}/Titulos/Cotizacion/Instrumentos', headers=self.headers)
-        return response.json()[titulo]
+        return make_response(dumps(response.json()))
