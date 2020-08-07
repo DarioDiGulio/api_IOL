f98ad08b2cb8e734b9f6e979921b2614e98d5849
Dario Di Gulio <ddigulio@technisys.com>
2020-08-06 22:55:21 -0300

    Flask no-cors

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
-from flask import Flask, request, abort
+from flask import Flask, request
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
 from routes.micuenta import *
 from routes.token import *
